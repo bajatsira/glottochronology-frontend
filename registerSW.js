d51bd424f6dfc6e62780d5e3ae1802d645794961
bajatsira <bajatsira@gmail.com>
2026-02-20 11:28:22 +0300

    Updates

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/glottochronology-frontend/sw.js', { scope: '/glottochronology-frontend/' })})}
